d81996b321a1de21c0d069371eca144483424ad2
sunilpowar <sunilpowarsp8468@gmail.com>
2022-01-28 19:50:03 +0530

    Adiing Welcome message

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to basic program");
+            BasicCorePrograms prob = new BasicCorePrograms();
+            prob.largest_num();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
